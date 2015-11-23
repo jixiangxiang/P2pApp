88e54ba81d31275c02429f53f23ae756e1fb605b
eric_jxx <jixiangxiang@infohold.com.cn>
2015-11-23 22:17:25 +0800

    开发oscar项目界面/add by jixiangxiang  2015-11-23

---
+++ oscar/src/main/java/com/example/eric/oscar/activity/OModifyLoginPwdActivity.java
@@ -0,0 +1,45 @@
+package com.example.eric.oscar.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.eric.oscar.R;
+import com.example.eric.oscar.common.BaseActivity;
+
+public class OModifyLoginPwdActivity extends BaseActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_omodify_login_pwd);
+    }
+
+    @Override
+    protected void initView() {
+        initTitleText(getString(R.string.title_activity_omodify_login_pwd), BaseActivity.TITLE_CENTER);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_omodify_login_pwd, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                showToastShort("点击了右侧按钮");
+                break;
+        }
+        return true;
+    }
+}
--- oscar/src/main/java/com/example/eric/oscar/fragment/OSelfFragment.java
+++ oscar/src/main/java/com/example/eric/oscar/fragment/OSelfFragment.java
@@ -5,18 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.eric.oscar.R;
+import com.example.eric.oscar.activity.OModifyLoginPwdActivity;
+import com.example.eric.oscar.activity.OSetPayPwdActivity;
+import com.example.eric.oscar.common.BaseActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link OSelfFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link OSelfFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OSelfFragment extends BaseFragment {
+public class OSelfFragment extends BaseFragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -25,6 +32,20 @@ public class OSelfFragment extends BaseFragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageView headImg;
+    private TextView username;
+    private TextView totalMoney;
+    private ImageView arrowRight;
+    private RelativeLayout headImgArea;
+    private TextView usernameText;
+    private RelativeLayout nameArea;
+    private RelativeLayout idArea;
+    private TextView verticalStatus;
+    private RelativeLayout validateArea;
+    private RelativeLayout loginPwdArea;
+    private RelativeLayout payPwdArea;
+    private RelativeLayout registPhoneArea;
+    private Button loginOutBtn;
 
     public OSelfFragment() {
         // Required empty public constructor
@@ -67,6 +88,34 @@ public class OSelfFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initialize(view);
+        loginPwdArea.setOnClickListener(this);
+        payPwdArea.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        if (v == loginPwdArea) {
+            ((BaseActivity) getActivity()).toActivity(OModifyLoginPwdActivity.class);
+        } else if (v == payPwdArea) {
+            ((BaseActivity) getActivity()).toActivity(OSetPayPwdActivity.class);
+        }
+    }
+
+    private void initialize(View view) {
+        headImg = (ImageView) view.findViewById(R.id.headImg);
+        username = (TextView) view.findViewById(R.id.username);
+        usernameText = (TextView) view.findViewById(R.id.usernameText);
+        totalMoney = (TextView) view.findViewById(R.id.totalMoney);
+        arrowRight = (ImageView) view.findViewById(R.id.arrowRight);
+        headImgArea = (RelativeLayout) view.findViewById(R.id.headImgArea);
+        nameArea = (RelativeLayout) view.findViewById(R.id.nameArea);
+        idArea = (RelativeLayout) view.findViewById(R.id.idArea);
+        verticalStatus = (TextView) view.findViewById(R.id.verticalStatus);
+        validateArea = (RelativeLayout) view.findViewById(R.id.validateArea);
+        loginPwdArea = (RelativeLayout) view.findViewById(R.id.loginPwdArea);
+        payPwdArea = (RelativeLayout) view.findViewById(R.id.payPwdArea);
+        registPhoneArea = (RelativeLayout) view.findViewById(R.id.registPhoneArea);
+        loginOutBtn = (Button) view.findViewById(R.id.loginOutBtn);
     }
 }
